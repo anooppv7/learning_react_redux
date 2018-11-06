89e0f7a851fa2f4fc0329900ee973e33e5d8f6d4
Anoop <anoop.pv7@gmail.com>
2018-11-06 23:07:50 +0530

    Add DownVote counter exactly similar to Upvote

--- 03-react-flux/src/App.js
+++ 03-react-flux/src/App.js
@@ -32,6 +32,32 @@ class UpVote extends React.Component {
 		);
 	}
 }
+
+class DownVote extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {counter: 0};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		this.setState(state => ({
+			counter: state.counter + 1
+    }));
+    this.props.decrementCounter();
+	}
+
+	render() {
+		return (
+			<button onClick={this.handleClick}>
+				{this.state.counter} Downvote
+			</button>
+		);
+	}
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -44,12 +70,20 @@ class App extends Component {
     });
   };
 
+  decrementCounter = () => {
+    this.setState({
+      overallCounter: this.state.overallCounter - 1 
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <CountTracker counter={this.state.overallCounter} />
         <br/>
         <UpVote counter="0" incrementCounter={this.incrementCounter}/>
+        <br/><br/>
+        <DownVote counter="0" decrementCounter={this.decrementCounter}/>
       </div>
     );
   }
