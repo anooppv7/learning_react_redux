6d6e62ee03e29a50f7a950bd6ef4bf6c5b9e4256
Anoop <anoop.pv7@gmail.com>
2018-11-08 13:29:01 +0530

    Step #1: for ease of use, move reducer into the render method

--- 04-flux-to-redux/src/App.js
+++ 04-flux-to-redux/src/App.js
@@ -30,6 +30,17 @@ class App extends React.Component {
 
   render() {
     window.addItem = this.addItem;
+
+    // #1
+    const reducer = function (state = [], action) {
+      switch (action.type) {
+        case "ADD_ITEM":
+          return [...state, action.payload]; // ideally should be object based, but array for easier understanding
+        default:
+          return state;
+      }
+    };
+
     return (
       <div> 
         Simplifying ...
